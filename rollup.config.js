b81a74a7d7d4f8125bbd2421b64d2001854166d6
Webber <webber@takken.io>
2020-10-20 22:04:45 +0200

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,35 @@
-"use strict";
+'use strict';
 
 import clear from 'rollup-plugin-clear';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
 import screeps from 'rollup-plugin-screeps';
+import typescript from 'rollup-plugin-typescript2';
 
-let cfg;
-const dest = process.env.DEST;
-if (!dest) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
-  throw new Error("Invalid upload destination");
+const destination = process.env.DEST;
+if (!destination) {
+  console.log('No destination specified - code will be compiled but not uploaded');
+}
+
+// eslint-disable-next-line
+const cfg = require('./screeps.json')[destination];
+if (cfg === null) {
+  throw new Error('Invalid upload destination');
 }
 
 export default {
-  input: "src/main.ts",
+  input: 'src/main.ts',
   output: {
-    file: "dist/main.js",
-    format: "cjs",
-    sourcemap: true
+    file: 'dist/main.js',
+    format: 'cjs',
+    sourcemap: true,
   },
 
   plugins: [
-    clear({ targets: ["dist"] }),
+    clear({ targets: ['dist'] }),
     resolve(),
     commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null})
-  ]
-}
+    typescript({ tsconfig: './tsconfig.json' }),
+    screeps({ config: cfg, dryRun: cfg == null }),
+  ],
+};
--- src/utils/ErrorMapper.ts
+++ src/utils/ErrorMapper.ts
@@ -1,4 +1,5 @@
-import { SourceMapConsumer } from "source-map";
+/* eslint-disable no-underscore-dangle */
+import { SourceMapConsumer } from 'source-map';
 
 export class ErrorMapper {
   // Cache consumer
@@ -6,7 +7,7 @@ export class ErrorMapper {
 
   public static get consumer(): SourceMapConsumer {
     if (this._consumer == null) {
-      this._consumer = new SourceMapConsumer(require("main.js.map"));
+      this._consumer = new SourceMapConsumer(require('main.js.map'));
     }
 
     return this._consumer;
@@ -36,10 +37,10 @@ export class ErrorMapper {
     let outStack = error.toString();
 
     while ((match = re.exec(stack))) {
-      if (match[2] === "main") {
+      if (match[2] === 'main') {
         const pos = this.consumer.originalPositionFor({
           column: parseInt(match[4], 10),
-          line: parseInt(match[3], 10)
+          line: parseInt(match[3], 10),
         });
 
         if (pos.line != null) {
@@ -74,7 +75,7 @@ export class ErrorMapper {
         loop();
       } catch (e) {
         if (e instanceof Error) {
-          if ("sim" in Game.rooms) {
+          if ('sim' in Game.rooms) {
             const message = `Source maps don't work in the simulator - displaying original error`;
             console.log(`<span style='color:red'>${message}<br>${_.escape(e.stack)}</span>`);
           } else {
